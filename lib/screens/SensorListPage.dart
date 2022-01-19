843f853993f22d13d39119abd0ed3d390662ca37
tshuenhau <tshuenhau@gmail.com>
2022-01-19 15:41:20 +0800

    refactored some widgets

--- lib/screens/SensorListPage.dart
+++ lib/screens/SensorListPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pycnomobile/screens/SensorSearchPage.dart';
+import 'package:pycnomobile/widgets/SensorsListTile.dart';
 
 class SensorListPage extends StatefulWidget {
   SensorListPage({Key? key}) : super(key: key);
@@ -57,61 +58,5 @@ class _SensorListPageState extends State<SensorListPage> {
         ),
       ]),
     );
-  }
-}
-
-class SensorsListTile extends StatefulWidget {
-  final String sensorName;
-  final String sensorSerial;
-  final List<String> availableGraphs; //! might need to change type
-  final String imageUrl;
-
-  SensorsListTile({
-    Key? key,
-    required this.sensorName,
-    required this.sensorSerial,
-    required this.availableGraphs,
-    required this.imageUrl,
-  }) : super(key: key);
-
-  @override
-  State<SensorsListTile> createState() => _SensorsListTileState();
-}
-
-class _SensorsListTileState extends State<SensorsListTile> {
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 5, vertical: 3),
-      child: ListTile(
-          title: Text(widget.sensorName),
-          subtitle: Text(widget.sensorSerial),
-          trailing: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                AvailableGraphsIcons(availableGraphs: widget.availableGraphs),
-                SizedBox(
-                    width: MediaQuery.of(context).size.width *
-                        (1 / 20)), // give it width
-                Image(image: NetworkImage(widget.imageUrl))
-              ])),
-    );
-  }
-}
-
-class AvailableGraphsIcons extends StatelessWidget {
-  final List<String>
-      availableGraphs; //! need to generate icons based on this. //! might need to change type
-  const AvailableGraphsIcons({Key? key, required this.availableGraphs})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(mainAxisSize: MainAxisSize.min, children: [
-      Icon(Icons.water),
-      Icon(Icons.light),
-      Icon(Icons.wb_sunny_outlined),
-    ]);
   }
 }
