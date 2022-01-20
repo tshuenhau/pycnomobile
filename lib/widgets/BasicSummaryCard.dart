d22237afa576c653ed11df5451d3435cc03b3495
tshuenhau <tshuenhau@gmail.com>
2022-01-20 22:59:17 +0800

    added inkwell to basicsummarycard

--- lib/widgets/BasicSummaryCard.dart
+++ lib/widgets/BasicSummaryCard.dart
@@ -19,34 +19,39 @@ class BasicSummaryCard extends StatelessWidget {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12.0),
         ),
-        child: Container(
-            width: MediaQuery.of(context).size.width * 1 / 2 - 20,
-            height: MediaQuery.of(context).size.height * 1 / 8 - 10,
-            child: Center(
-              child: Row(children: [
-                SizedBox(width: MediaQuery.of(context).size.width * 3.5 / 100),
-                Icon(
-                  icon,
-                  size: MediaQuery.of(context).size.width * 10 / 100,
-                  color: color,
-                ),
-                SizedBox(width: MediaQuery.of(context).size.width * 2 / 100),
-                Row(
-                  children: [
-                    Text(
-                      value.toString(),
-                      style: TextStyle(
-                          fontSize:
-                              MediaQuery.of(context).size.width * 7 / 100),
-                    ),
-                    Container(
-                        padding: EdgeInsets.only(
-                            top: MediaQuery.of(context).size.height * 1 / 100),
-                        child: Text(" " + unit)),
-                  ],
-                ),
-                //trailing: Text(" °C")
-              ]),
-            )));
+        child: InkWell(
+          onTap: () => {},
+          child: Container(
+              width: MediaQuery.of(context).size.width * 1 / 2 - 20,
+              height: MediaQuery.of(context).size.height * 1 / 8 - 10,
+              child: Center(
+                child: Row(children: [
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width * 3.5 / 100),
+                  Icon(
+                    icon,
+                    size: MediaQuery.of(context).size.width * 10 / 100,
+                    color: color,
+                  ),
+                  SizedBox(width: MediaQuery.of(context).size.width * 2 / 100),
+                  Row(
+                    children: [
+                      Text(
+                        value.toString(),
+                        style: TextStyle(
+                            fontSize:
+                                MediaQuery.of(context).size.width * 7 / 100),
+                      ),
+                      Container(
+                          padding: EdgeInsets.only(
+                              top:
+                                  MediaQuery.of(context).size.height * 1 / 100),
+                          child: Text(" " + unit)),
+                    ],
+                  ),
+                  //trailing: Text(" °C")
+                ]),
+              )),
+        ));
   }
 }
