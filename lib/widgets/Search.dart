0d8e8625e565960eaaee99a96781f44aab2072f9
tshuenhau <tshuenhau@gmail.com>
2022-02-09 15:10:37 +0800

    reverted search widget styling

--- lib/widgets/Search.dart
+++ lib/widgets/Search.dart
@@ -18,41 +18,45 @@ class Search extends StatelessWidget {
     final TextEditingController textController = new TextEditingController();
 
     textController.text = controller.searchController.value;
-    return Container(
-      height: MediaQuery.of(context).size.height * 5 / 100,
-      margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: Colors.white,
-        border: Border.all(color: Colors.black26),
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: Obx(
-        () => TextField(
-          controller: textController,
-          decoration: InputDecoration(
-            prefixIcon: Icon(Icons.search),
-            suffixIcon: controller.searchController.value.isNotEmpty
-                ? IconButton(
-                    icon: Icon(Icons.clear),
-                    onPressed: () {
-                      textController.clear();
-                      controller.searchController.value = "";
-                      controller.searchListOfSensors();
-                      FocusScope.of(context).requestFocus(FocusNode());
-                    },
-                  )
-                : null,
-            hintText: hintText,
-            border: InputBorder.none,
+    return Card(
+      //elevation: 2,
+      //margin: const EdgeInsets.fromLTRB(4, 4, 0, 4),
+      child: Container(
+        height: MediaQuery.of(context).size.height * 5 / 100,
+        margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.white,
+          border: Border.all(color: Colors.black26),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Obx(
+          () => TextField(
+            controller: textController,
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search),
+              suffixIcon: controller.searchController.value.isNotEmpty
+                  ? IconButton(
+                      icon: Icon(Icons.clear),
+                      onPressed: () {
+                        textController.clear();
+                        controller.searchController.value = "";
+                        controller.searchListOfSensors();
+                        FocusScope.of(context).requestFocus(FocusNode());
+                      },
+                    )
+                  : null,
+              hintText: hintText,
+              border: InputBorder.none,
+            ),
+            style: controller.searchController.value.isEmpty
+                ? styleHint
+                : styleActive,
+            onChanged: (text) {
+              controller.searchController.value = text;
+              controller.searchListOfSensors();
+            },
           ),
-          style: controller.searchController.value.isEmpty
-              ? styleHint
-              : styleActive,
-          onChanged: (text) {
-            controller.searchController.value = text;
-            controller.searchListOfSensors();
-          },
         ),
       ),
     );
