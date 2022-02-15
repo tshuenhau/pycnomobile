7e8b63c1306633144465a933721a98c178058e21
tshuenhau <tshuenhau@gmail.com>
2022-02-15 15:43:15 +0800

    commented out error file

--- lib/widgets/DateRangeSelector.dart
+++ lib/widgets/DateRangeSelector.dart
@@ -1,33 +1,33 @@
-import 'package:flutter/material.dart';
-import 'package:pycnomobile/builders/SensorGraphsBuilder.dart';
-import 'package:pycnomobile/model/TimeSeries.dart';
-import 'package:pycnomobile/theme/GlobalTheme.dart';
+// import 'package:flutter/material.dart';
+// import 'package:pycnomobile/builders/SensorGraphsBuilder.dart';
+// import 'package:pycnomobile/model/TimeSeries.dart';
+// import 'package:pycnomobile/theme/GlobalTheme.dart';
 
-Widget DateRangeSelector(BuildContext context) {
-  return Theme(
-    data: ThemeData(
-        colorScheme: globalTheme.colorScheme.copyWith(
-            primary: globalTheme.colorScheme.secondary,
-            secondary: globalTheme.colorScheme.primary)),
-    child: Builder(
-      builder: (context) => ElevatedButton(
-          onPressed: () async {
-            DateTimeRange? _newDateRange = await showDateRangePicker(
-                context: context,
-                firstDate: DateTime(1800),
-                lastDate: DateTime(3000));
+// Widget DateRangeSelector(BuildContext context) {
+//   return Theme(
+//     data: ThemeData(
+//         colorScheme: globalTheme.colorScheme.copyWith(
+//             primary: globalTheme.colorScheme.secondary,
+//             secondary: globalTheme.colorScheme.primary)),
+//     child: Builder(
+//       builder: (context) => ElevatedButton(
+//           onPressed: () async {
+//             DateTimeRange? _newDateRange = await showDateRangePicker(
+//                 context: context,
+//                 firstDate: DateTime(1800),
+//                 lastDate: DateTime(3000));
 
-            if (_newDateRange != null) {
-              List<TimeSeries>? result = await getGraphsForTimeRange(
-                  _newDateRange, widget.sensor, widget.functions);
-              setState(() {
-                graphs = result!;
-              });
+//             if (_newDateRange != null) {
+//               List<TimeSeries>? result = await getGraphsForTimeRange(
+//                   _newDateRange, widget.sensor, widget.functions);
+//               setState(() {
+//                 graphs = result!;
+//               });
 
-              //buildSensorGraphs(context, sensor, functions, _newDateRange);
-            }
-          },
-          child: Icon(Icons.today)),
-    ),
-  );
-}
+//               //buildSensorGraphs(context, sensor, functions, _newDateRange);
+//             }
+//           },
+//           child: Icon(Icons.today)),
+//     ),
+//   );
+// }
