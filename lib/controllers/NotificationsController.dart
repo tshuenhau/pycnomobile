df9ffda4bca06e34af30d5e37ac9e128a4ee8135
ChuangZheQuan <zhequan7chuang@gmail.com>
2022-02-22 15:38:17 +0800

    Make alerts observable

--- lib/controllers/NotificationsController.dart
+++ lib/controllers/NotificationsController.dart
@@ -7,30 +7,30 @@ import 'package:pycnomobile/model/NotificationData.dart';
 class NotificationsController extends GetxController {
   AuthController authController = Get.find();
   RxList<NotificationData> notifications = RxList.empty();
-  int alertCounter = 0;
-  bool isSevere = false;
+  Rx<int> alertCounter = 0.obs;
+  Rx<bool> isSevere = false.obs;
 
   Future<void> getNotifications() async {
-    alertCounter = 0;
-    isSevere = false;
+    alertCounter.value = 0;
+    isSevere.value = false;
+    notifications.clear();
     final response = await http.get(Uri.parse(
         'https://stage.pycno.co.uk/api/v2/notifications.json?TK=${authController.token}'));
     print(
         'https://stage.pycno.co.uk/api/v2/notifications.json?TK=${authController.token}');
     if (response.statusCode >= 200) {
       var body = jsonDecode(response.body);
-      print(body);
-      print(body.length);
       for (var i = 0; i < body.length; i++) {
         NotificationData notif = NotificationData.fromJson(body[i]);
         if (notif.severity > 0 && notif.state == 0) {
           notifications.add(notif);
           if (notif.severity > 3) {
-            isSevere = true;
+            isSevere.value = true;
           }
-          alertCounter++;
+          alertCounter.value++;
         }
       }
+      print(alertCounter);
       print(notifications);
     } else {
       throw Exception("Unable to get notifications");
