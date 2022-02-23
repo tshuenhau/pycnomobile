2cc610a251299059a55b47503c6ec65be9d854ad
tshuenhau <tshuenhau@gmail.com>
2022-02-23 16:41:24 +0800

    added debug print for alerts time ago

--- lib/widgets/AlertListTile.dart
+++ lib/widgets/AlertListTile.dart
@@ -17,6 +17,17 @@ class AlertListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print(notification.desc);
+    print("notification time UTC: " +
+        DateTime.fromMillisecondsSinceEpoch(notification.epoch, isUtc: true)
+            .toUtc()
+            .toString());
+    print("notification time Local: " +
+        DateTime.fromMillisecondsSinceEpoch(notification.epoch, isUtc: true)
+            .toLocal()
+            .toString());
+    print(DateTime.fromMillisecondsSinceEpoch(notification.epoch, isUtc: true)
+        .timeZoneOffset);
+    print(DateTime.now());
     return Dismissible(
       key: Key(this.notification.id.toString()),
       onDismissed: (notificationId) {
@@ -64,7 +75,9 @@ class AlertListTile extends StatelessWidget {
                     subtitle: Text(notification.uid),
                     trailing: Text(
                         timeago.format(DateTime.fromMillisecondsSinceEpoch(
-                            notification.epoch)),
+                                notification.epoch,
+                                isUtc: true)
+                            .toLocal()),
                         style: TextStyle(
                             fontSize:
                                 MediaQuery.of(context).size.width * 3 / 100))),
