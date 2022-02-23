7d0c07a1ad16c8556eeb9a525fa67b025807d2fa
tshuenhau <tshuenhau@gmail.com>
2022-02-23 16:05:57 +0800

    fixed refresh on alerts

--- lib/screens/AlertsPage.dart
+++ lib/screens/AlertsPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pycnomobile/builders/AlertsBuilder.dart';
+import 'package:pycnomobile/controllers/NotificationsController.dart';
 import 'package:pycnomobile/theme/GlobalTheme.dart';
 import 'package:pycnomobile/widgets/AlertListTile.dart';
 import 'package:get/get.dart';
@@ -12,7 +13,14 @@ class AlertsPage extends StatefulWidget {
 }
 
 class _AlertsPageState extends State<AlertsPage> {
-  // List<Widget> unreadNotifications = buildAlerts();
+  List<Widget> unreadNotifications = buildAlerts(isRead: false);
+  List<Widget> readNotifications = buildAlerts(isRead: true);
+  final NotificationsController notificationsController =
+      Get.put(NotificationsController());
+  Future _refreshData() async {
+    await notificationsController.getNotifications();
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -59,16 +67,11 @@ class _AlertsPageState extends State<AlertsPage> {
             padding: EdgeInsets.only(
                 top: MediaQuery.of(context).size.height * 0.5 / 100),
             child: TabBarView(children: [
-              Obx(() => ListView(children: buildAlerts(isRead: false))),
+              RefreshIndicator(
+                  onRefresh: _refreshData,
+                  child: Obx(
+                      () => ListView(children: buildAlerts(isRead: false)))),
               Obx(() => ListView(children: buildAlerts(isRead: true))),
-              // RefreshIndicator(
-              //     onRefresh: () async {
-              //       setState(() {
-              //         unreadNotifications = buildAlerts(isRead: false);
-              //       });
-              //     },
-              //     child: ListView(children: unreadNotifications)),
-              // ListView(children: buildAlerts()),
             ]),
           ),
         ));
