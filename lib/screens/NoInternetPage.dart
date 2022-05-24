ae549a71e2915e4296019e1e641d04275b6bba86
tshuenhau <tshuenhau@gmail.com>
2022-05-24 15:30:30 +0800

    styled no internet page

--- lib/screens/NoInternetPage.dart
+++ lib/screens/NoInternetPage.dart
@@ -6,28 +6,48 @@ class NoInternetPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Column(
-      children: [
-        Text("No Internet!"),
-        Text("Check you connection and try again!"),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            elevation: 1,
-            primary: Colors.green,
-            onPrimary: Colors.white,
-            onSurface: Theme.of(context).colorScheme.tertiary,
-          ),
-          child: Text('Login',
-              style: TextStyle(
-                  fontFamily: 'nulshock',
-                  fontSize: MediaQuery.of(context).size.width * 5 / 100)),
-          onPressed: () {
-            Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) => LoginPage()));
-          },
-        ),
-      ],
-    ));
+    return Center(
+      child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // SizedBox(height: MediaQuery.of(context).size.height * 1.5 / 100),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 12 / 100,
+                width: MediaQuery.of(context).size.width * 18 / 100,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 1,
+                    primary: Colors.black,
+                    onPrimary: Colors.white,
+                    onSurface: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.transparent)),
+                  ),
+                  child: Icon(Icons.refresh,
+                      size: MediaQuery.of(context).size.width * 8 / 100),
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => LoginPage()));
+                  },
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 1.5 / 100),
+              Text("No Internet!",
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontFamily: 'GothamRounded',
+                      fontSize: MediaQuery.of(context).size.width * 3.5 / 100)),
+              Text("Check you connection and try again!",
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontFamily: 'GothamRounded',
+                      fontSize: MediaQuery.of(context).size.width * 3.5 / 100)),
+            ],
+          )),
+    );
   }
 }
